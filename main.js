29a7e5090fd07872b2d045e8e130d565e751d2dc
KennethLics <licuanakennethlloyd05@gmail.com>
2024-11-23 18:22:01 +0800

    added method popup

--- main.js
+++ main.js
@@ -76,4 +76,9 @@ class LeafletMap {
         const marker = L.marker([lat, lng]).addTo(this.map);
         marker.bindPopup(message);
     }
+
+    updateMarkerPopup(marker, message) {
+        const count = this.markerCounts[message];
+        marker.bindPopup(`${message}<br>Attendance logs: ${count}`).openPopup();
+    }
 }
