e09621c7c084bf3bbae15fd532ac22933f0764e0
KennethLics <licuanakennethlloyd05@gmail.com>
2024-11-23 18:23:29 +0800

    added method loadmarkersfromJson

--- main.js
+++ main.js
@@ -81,4 +81,15 @@ class LeafletMap {
         const count = this.markerCounts[message];
         marker.bindPopup(`${message}<br>Attendance logs: ${count}`).openPopup();
     }
-}
+
+    loadMarkersFromJson(url) {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                data.forEach(marker => {
+                    this.addMarker(marker.latitude, marker.longitude, marker.message);
+                });
+            })
+            .catch(error => console.error('Error loading markers:', error));
+        }
+    }
